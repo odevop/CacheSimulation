b194a9e93e202a131d37936f535e7733aea31c11
ararity <epicbwo@gmail.com>
2023-11-18 21:46:01 -0500

    Comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 /************************************************************************************************/
-/*                                   Cache Simultor                                             */
+/*                                   Cache Simulator                                            */
 /*                                                                                              */
 /*                                     Blake Owen                                               */
 /*                                                                                              */
 /*       This program is designed to simulate various cache configurations. It takes a trace    */
 /*       file as input (with memory reads and writes included) and prompts the user if they     */
-/*       would like to run the automated tests. If not, the user is prompted for specific       */
-/*       system specification they would like to simulate.                                      */
+/*       would like to run the automated tests. If not, the user is prompted for the system     */
+/*       specifications they would like to simulate.                                            */
 /************************************************************************************************/
 
 #include <iostream>
